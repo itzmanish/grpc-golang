510c374449441a9506d7ca4e525bc65a27a54ab0
Manish <itzmanish108@gmail.com>
2020-01-08 13:51:08 +0530

    Fixed: some ssl shits

--- ssl/gen_cert.sh
+++ ssl/gen_cert.sh
@@ -6,18 +6,20 @@
 CA_PASSWORD=ThisShouldBeStrongButString
 
 # Generate the root certificate authority key with the set password
-openssl genrsa -des3 -passout pass:$CA_PASSWORD -out localhostCA.key 2048
+openssl genrsa -des3 -passout pass:$CA_PASSWORD -out CA.key 4096
 
 # Generate a root-certificate based on the root-key for importing to browsers.
-openssl req -x509 -new -nodes -key localhostCA.key -passin pass:$CA_PASSWORD -config localhostCA.conf -sha256 -days 1825 -out localhostCA.pem
+openssl req -x509 -new -nodes -key CA.key -passin pass:$CA_PASSWORD -config localhostCA.conf -sha256 -days 1825 -out CA.crt
 
 # Generate a new private key
-openssl genrsa -out localhost.key 2048
+openssl genrsa -out server.key 4096
 
 # Generate a Certificate Signing Request (CSR) based on that private key (reusing the
 # localhostCA.conf details)
-openssl req -new -key localhost.key -out localhost.csr -config localhostCA.conf
+openssl req -new -key server.key -out server.csr -config localhostCA.conf
 
 # Create the certificate for the webserver to serve using the localhost.conf config.
-openssl x509 -req -in localhost.csr -CA localhostCA.pem -CAkey localhostCA.key -CAcreateserial \
--out localhost.crt -days 1024 -sha256 -extfile localhost.conf -passin pass:$CA_PASSWORD
+openssl x509 -req -in server.csr -CA CA.crt -CAkey CA.key -CAcreateserial \
+-out server.crt -days 1024 -sha256 -extfile localhost.conf -passin pass:$CA_PASSWORD
+
+openssl pkcs8 -topk8 -nocrypt -passin pass:$CA_PASSWORD -in server.key -out server.pem
