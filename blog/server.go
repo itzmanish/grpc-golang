7fb938e868150a367a746859305d77f9c06dab22
Manish <itzmanish108@gmail.com>
2020-01-12 00:50:28 +0530

    Added: mongoDB drivers to interact with mlab mongodb database.

--- blog/server.go
+++ blog/server.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"google.golang.org/grpc/reflection"
 
 	"github.com/itzmanish/grpc-go/blog/blogpb"
 	"google.golang.org/grpc"
 )
 
+var collection *mongo.Collection
+
 type server struct{}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
 
 func main() {
 	// For getting location of raised error or warnings with line
@@ -24,6 +38,17 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
+	fmt.Println("Connecting to mongoDB...")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://rudra:rudra108@ds361488.mlab.com:61488"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	collection = client.Database("crudgrpc").Collection("blog")
 	opts := []grpc.ServerOption{}
 	s := grpc.NewServer(opts...)
 
@@ -46,5 +71,5 @@ func main() {
 	fmt.Printf("\nStopping server..... \n ")
 	s.Stop()
 	lis.Close()
-
+	client.Disconnect(context.TODO())
 }
